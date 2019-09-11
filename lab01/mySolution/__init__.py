defa37395bfd2dbf2ad13998a5891b525652ed46
KingJeremyNg <jeremy97ng@gmail.com>
2019-09-11 11:51:34 -0700

    Rewrote monkeytypist and generate

--- lab01/mySolution/__init__.py
+++ lab01/mySolution/__init__.py
@@ -1,29 +1,39 @@
+import random
+
 def generate(lenS, LenA, alphabet) :
-    target = ""
+    string = ""
+    for x in range(lenS) :
+        string += alphabet[random.randint(0, len(LenA) + 1) - 1]
+    return string
+    
 
 def calcScore(newStr, target) :
     for i in range(target) :
         str = generate(lenS, LenA, alphabet)
-        print("%s %d" % ("".join(str), calcScore(str, target)))
+        print("%s\t%d" % ("".join(str), calcScore(str, target)))
 
 def monkeyTypist() :
     target = list("methinks it is like a weasel")
-    lenS = len(target)
     alphabet = list("abcdefghijklmnopqrstuvwxyz ")
+
+    lenS = len(target)
     lenA = len(alphabet)
     epoch = 0
+
     bestStr = generate(lenS, lenA, alphabet)
     bestScore = calcScore(bestStr, target)
+
     print("String Score")
+
     while bestScore < 100 :
         if bestScore == 100 :
             break
         newStr = generate(lenS, lenA, alphabet)
         newScore = calcScore(newStr, target)
-        if newScore > bestScore:
+        if newScore > bestscore :
             bestScore = newScore
             bestStr = newStr
-        if epoch == 100000:
-            print("%s %d" % ("".join(bestStr), bestScore))
+        if epoch == 100000 :
+            print("%s\t\t\t%d" % ("".join(bestStr), bestScore))
             epoch = 0
-        epoch = epoch + 1
+        epoch += 1
