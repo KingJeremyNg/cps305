83afab0799d4e905f39420f5e2e72ac77ffde362
KingJeremyNg <jeremy97ng@gmail.com>
2019-09-11 16:29:51 -0300

    Rewrote 3 functions

--- lab01/mySolution/__init__.py
+++ lab01/mySolution/__init__.py
@@ -1,39 +1,41 @@
 import random
 
-def generate(lenS, LenA, alphabet) :
-    string = ""
-    for x in range(lenS) :
-        string += alphabet[random.randint(0, len(LenA) + 1) - 1]
-    return string
-    
-
-def calcScore(newStr, target) :
-    for i in range(target) :
-        str = generate(lenS, LenA, alphabet)
-        print("%s\t%d" % ("".join(str), calcScore(str, target)))
-
 def monkeyTypist() :
+
     target = list("methinks it is like a weasel")
     alphabet = list("abcdefghijklmnopqrstuvwxyz ")
 
     lenS = len(target)
-    lenA = len(alphabet)
-    epoch = 0
 
-    bestStr = generate(lenS, lenA, alphabet)
+    bestStr = generate(lenS, alphabet)
     bestScore = calcScore(bestStr, target)
 
-    print("String Score")
+    print("String\t\t\t\t\tScore")
 
     while bestScore < 100 :
         if bestScore == 100 :
             break
-        newStr = generate(lenS, lenA, alphabet)
+        newStr = generate(lenS, alphabet)
         newScore = calcScore(newStr, target)
-        if newScore > bestscore :
+        if newScore > bestScore :
             bestScore = newScore
             bestStr = newStr
         if epoch == 100000 :
-            print("%s\t\t\t%d" % ("".join(bestStr), bestScore))
+            print ("%s\t\t%d" % ("".join(bestStr), bestScore))
             epoch = 0
         epoch += 1
+
+def generate(lenS, alphabet) :
+    string = []
+    for i in range(lenS) :
+        string.append(alphabet[random.randint(0, len(alphabet) - 1)])
+    return string
+
+def calcScore(newStr, target) :
+    count = 0
+    for i in range(len(target)) :
+        if newStr[i] == target[i] :
+            count += 1
+    return (count (float) / target (float))
+
+monkeyTypist()
