249d141d6db1c07b15e6970961caa5de393b63f8
KingJeremyNg <jeremy97ng@gmail.com>
2019-09-19 10:29:53 -0400

    Done lab02. Timeit is a pain in the ass

--- lab02/mySolution/deleteTime.py
+++ lab02/mySolution/deleteTime.py
@@ -5,22 +5,19 @@ import random
 timeDeleteDict = []
 timeDeleteList = []
 count = []
-size = 1000000
+size = 2000000
 
-for i in range(0, size, 50000) :
+for i in range(10000, size, 50000) :
     rand = random.randint(0, i)
     dic = {x: random.randint(0, i) for x in range(i)}
     lis = [x for x in range(i)]
     keys = list(dic.keys())
 
-    print(dic)
-    print(keys)
-
     dictTimer = timeit.Timer("del dic[keys[random.randint(0, len(keys) - 1)]]", "from __main__ import dic, keys, random")
-    listTimer = timeit.Timer("del lis[random.randint(0, i)]", "from __main__ import dic, random")
+    listTimer = timeit.Timer("del lis[random.randint(0, i)]", "from __main__ import lis, random, i")
     
-    timeDeleteDict.append(dictTimer.timeit(number = 100))
-    timeDeleteList.append(listTimer.timeit(number = 100))
+    timeDeleteDict.append(dictTimer.timeit(number = 10))
+    timeDeleteList.append(listTimer.timeit(number = 10))
     count.append(i)
 
 plot.scatter(count, timeDeleteDict, color = 'r', marker = '+')
